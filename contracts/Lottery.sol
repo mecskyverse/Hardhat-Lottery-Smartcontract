2dcc977df96d7d808409110edd28fc7e7c754a8c
Aakash <vermaaakash1000@gmail.com>
2022-09-01 13:08:00 +0530

    Chainlink VRFV2 added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-Raffle__NotEnoughEthENtered();
+error Raffle__NotEnoughEthENtered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /*State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -14,20 +14,30 @@ contract Raffle {
     event RaffleEnter(address indexed player);
 
     /*Constructor*/
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCordinatorV2) {
         i_entranceFee = entranceFee;
     }
-    function enterRaffle() public payable{
-        if(msg.value<i_entranceFee){
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthENtered();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    //View / Pure Functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
+
     function getPlayers(uint256 index) public view returns (address) {
         return s_players[index];
     }
