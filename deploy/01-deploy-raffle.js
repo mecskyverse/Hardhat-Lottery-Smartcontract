88b73bc7759470770a1033483b5139a9f8b82c0b
Aakash <vermaaakash1000@gmail.com>
2022-09-13 12:06:32 +0530

    deploy script mock added

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,6 +1,12 @@
-const { deployments } = require("hardhat")
+const { deployments, network } = require("hardhat")
 
 module.exports = async function ({ getNamedAccounts, deployements }) {
     const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts
+    const { deployer } = await getNamedAccounts()
+    const raffe = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmattions || 1,
+    })
 }
