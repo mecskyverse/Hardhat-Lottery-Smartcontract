fe580d39db1c6a9c155d53880ba43b26e57c453a
Aakash <vermaaakash1000@gmail.com>
2022-09-13 10:34:37 +0530

    Networks and acoounts added

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,22 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const RINKEBY_RPC_URL = process.env.rinkebyRpcUrl
+const PRIVATE_KEY = process.env.rinkebyKey
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            chainId: 4,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            blockConfirmations: 6,
+        },
+    },
     solidity: "0.8.9",
     namedAccounts: {
         deployer: {
