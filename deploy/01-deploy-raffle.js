c82bda9a0af6a428f4407cd22378c38612a07d21
Aakash <vermaaakash1000@gmail.com>
2022-09-14 12:30:17 +0530

    Deploy script args added

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,11 +1,20 @@
-const { deployments, network } = require("hardhat")
+const { deployments, network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async function ({ getNamedAccounts, deployements }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const raffe = await deploy("Raffle", {
+    let vrfCoordinatorV2Address
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId][vrfCoordinatorV2]
+    }
+    const args = []
+    const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmattions || 1,
     })
