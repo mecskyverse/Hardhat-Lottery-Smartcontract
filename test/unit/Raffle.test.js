64b139b5fe482adc4b87704c91dbac2a3f43fabf
Aakash <vermaaakash1000@gmail.com>
2022-09-16 13:25:36 +0530

    Not enough ETH reverted

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,5 +1,4 @@
-const { inputToConfig } = require("@ethereum-waffle/compiler")
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
@@ -21,6 +20,13 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   const interval = await raffle.getInterval()
                   assert.equal(raffleState.toString(), "0")
                   assert.equal(interval.toString(), networkConfig[chainId].interval)
+              })
+          })
+          describe("enterRaffle", async function () {
+              it("reverts when you don't pay enough", async function () {
+                  await expect(raffle.enterRaffle()).to.be.revertedWith(
+                      "Raffle__NotEnoughEthENtered"
+                  )
               })
           })
       })
