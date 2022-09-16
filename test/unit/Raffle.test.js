2b0e49b908781f3cbaae23def6a4d9a7244b1214
Aakash <vermaaakash1000@gmail.com>
2022-09-16 13:39:59 +0530

    TEST: players record

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,13 +5,15 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle Unit Test", async function () {
-          let raffle, vrfCoordinatorV2Mock
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
+
           const chainId = network.config.chainId
           beforeEach(async function () {
-              const { deployer } = getNamedAccounts()
+              deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              raffleEntranceFee = raffle.getEntranceFee()
           })
           describe("constructor", async function () {
               it("initializes the raffle correctly", async function () {
@@ -27,6 +29,11 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   await expect(raffle.enterRaffle()).to.be.revertedWith(
                       "Raffle__NotEnoughEthENtered"
                   )
+              })
+              it("records players when they enter", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const playerFromContract = await raffle.getPlayers(0)
+                  assert.equal(playerFromContract, deployer)
               })
           })
       })
