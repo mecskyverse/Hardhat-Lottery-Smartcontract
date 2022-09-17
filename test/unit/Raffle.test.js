131373ad7b90e06d53c4189b9cc8935cddcb3a54
Aakash <vermaaakash1000@gmail.com>
2022-09-17 13:16:40 +0530

    Checking calculating state

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,7 +5,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle Unit Test", async function () {
-          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval
 
           const chainId = network.config.chainId
           beforeEach(async function () {
@@ -14,12 +14,13 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
               raffleEntranceFee = raffle.getEntranceFee()
+              interval = await raffle.getInterval()
           })
           describe("constructor", async function () {
               it("initializes the raffle correctly", async function () {
                   //Ideally we make our test have just 1 assert per it
                   const raffleState = await raffle.getRaffleState()
-                  const interval = await raffle.getInterval()
+
                   assert.equal(raffleState.toString(), "0")
                   assert.equal(interval.toString(), networkConfig[chainId].interval)
               })
@@ -39,6 +40,15 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(
                       raffle,
                       "RaffleEnter"
+                  )
+              })
+              it("gives the error while raffle is in calculating state", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  await raffle.performUpkeep([])
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.revertedWith(
+                      "Raffle__NotOpen"
                   )
               })
           })
